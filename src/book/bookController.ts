ccb126ea2eca28895b8e9e5585de0f3d7e568b9a
Rahu1kr <88312053+Rahu1kr@users.noreply.github.com>
2024-08-12 13:09:40 +0530

    add delete book and cors support

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -174,5 +174,42 @@ const getSingleBook = async (req: Request, res: Response, next: NextFunction) =>
     }
 };
 
+const deleteBook = async (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
 
-export {createBook, updateBook, listBooks, getSingleBook};
+    const book = await bookModel.findOne({_id: bookId})
+    if(!book){
+        return next(createHttpError(404, "Book not found"))
+    }
+
+     // Check access
+     const _req = req as AuthRequest;
+     if (book.author.toString() !== _req.userId) {
+         return next(createHttpError(403, "You can not delete others book."));
+     }
+
+
+     const coverFileSplits = book.coverImage.split("/");
+     const coverImagePublicId = coverFileSplits.at(-2) + "/" + coverFileSplits.at(-1)?.split(".").at(-2);
+
+     const bookFileSplits = book.file.split('/');
+     const bookFilePublicId = bookFileSplits.at(-2) + "/" + bookFileSplits.at(-1);
+     console.log("bookFilePublicId", bookFilePublicId);
+
+    //  todo add try catch error block
+   try {
+    await cloudinary.uploader.destroy(coverImagePublicId);
+    await cloudinary.uploader.destroy(bookFilePublicId,{
+        resource_type: "raw",
+    });
+
+   } catch (error) {
+        return next(createHttpError(500, "An error occurred while deleting files."))
+   }
+    await bookModel.deleteOne({_id: bookId});
+
+    return res.sendStatus(204);
+};
+
+
+export {createBook, updateBook, listBooks, getSingleBook, deleteBook};
